2e2bb63e869932672459c55da107e27c4b3de5cd
Mikael Lindeman <mikael.kalajalindeman@student.kyh.se>
2023-08-07 14:14:04 +0200

    App tsx setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
+import { Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import GamePage from "./pages/GamePage/GamePage";
+import ResultPage from "./pages/ResultPage/ResultPage";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" Component={LandingPage} />
+          <Route path="/game" Component={GamePage} />
+          <Route path="/result" Component={ResultPage} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
