eb628a0cf439e94be04c58f7fe2004081d9c7cbc
Mikael Lindeman <mikael.kalajalindeman@student.kyh.se>
2023-08-18 14:29:06 +0200

    Added about page infotext

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -3,7 +3,36 @@ import React from "react";
 function AboutPage() {
   return (
     <div>
-      <h1>About</h1>
+      <h1>
+        {/*         
+Sample text / Might be used for the finishd product
+        About LillaLingo LillaLingo is an innovative language comprehension
+        tool, inspired by real-world practices in speech therapy. Designed
+        primarily for children aged 3-5, LillaLingo provides a captivating way
+        for kids to improve their linguistic skills and comprehension. The
+        Inspiration The idea for LillaLingo was born from a personal experience.
+        Every visit to the speech therapist with my son involved a unique test -
+        a series of images and a corresponding statement. For example, a
+        statement like "John is eating a red apple" would be paired with images
+        of a girl eating a red apple, a boy eating a blue apple, and a boy
+        eating a red apple. The challenge? Identifying the correct image that
+        matches the statement. This method, widely recognized in speech therapy,
+        gauges a child's understanding of language, prepositions, and context.
+        Why LillaLingo? Real-World Application: LillaLingo's methodology is
+        grounded in proven techniques used by speech therapists worldwide. It
+        bridges the gap between therapy sessions and daily practice at home.
+        Progress Tracking: After a series of questions, parents and guardians
+        can monitor a child's progress, offering insights into their linguistic
+        understanding and development. Engaging & Interactive: With vibrant
+        visuals and intuitive gameplay, LillaLingo turns learning into a fun
+        game. Kids not only learn but are motivated to score better each time.
+        Adaptable Levels: While primarily designed for younger children,
+        LillaLingo's adaptive difficulty ensures it remains challenging and
+        beneficial for older kids as well. LillaLingo's Promise Language is the
+        key to understanding our world. LillaLingo endeavors to unlock this
+        world for our young ones, ensuring they grasp and comprehend the beauty
+        and intricacy of language, one game at a time. */}
+      </h1>
     </div>
   );
 }
