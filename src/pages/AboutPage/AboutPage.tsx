790093b6f52e628058e95299afde198b17b3b5ba
Mikael Lindeman <mikael.kalajalindeman@student.kyh.se>
2023-08-23 15:45:14 +0200

    Added about me page and text loadings

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,5 +1,7 @@
 import "./AboutPage.scss";
 import useSanityFetch from "../../components/Hooks/useSanityFetch";
+import { buttonVariants } from "../../animations/variants";
+import { motion } from "framer-motion";
 
 function AboutPage() {
   const query = `*[_type == "about"]{title, description}`;
@@ -10,12 +12,30 @@ function AboutPage() {
 
   return (
     <div className="about-page">
-      {data.map((entry: any, index: number) => (
-        <div key={index}>
-          <h1>{entry.title}</h1>
-          <p>{entry.description}</p>
-        </div>
-      ))}
+      <div className="about-page__text">
+        {data.map((entry: any, index: number) => (
+          <div key={index}>
+            <motion.h1
+              className="about-page__title"
+              custom={1 * index + 1}
+              variants={buttonVariants}
+              initial="hidden"
+              animate="visible"
+            >
+              {entry.title}
+            </motion.h1>
+
+            <motion.p
+              custom={1 * index + 2}
+              variants={buttonVariants}
+              initial="hidden"
+              animate="visible"
+            >
+              {entry.description}
+            </motion.p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
